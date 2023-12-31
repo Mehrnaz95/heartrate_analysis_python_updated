5e113688536dabd0d9b1487ad184e3b973fccbbf
Mehrnaz95 <85008033+Mehrnaz95@users.noreply.github.com>
2023-12-30 18:04:27 -0800

    Update peakdetection.py

--- heartpy/peakdetection.py
+++ heartpy/peakdetection.py
@@ -282,7 +282,7 @@ def fit_peaks(hrdata, rol_mean, sample_rate, bpmmin=40, bpmmax=180, working_data
 
     # moving average values to test
     # ma_perc_list = [0,1,2,3,4, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 150, 200, 300]
-    ma_perc_list = [2,3,4, 5, 10, 15]
+    ma_perc_list = [2,3,4, 5, 10, 15,20]
 
 
     rrsd = []
